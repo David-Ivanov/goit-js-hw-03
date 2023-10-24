4d3b15374c3da664ab4a830852fb5625165ae02c
Dav1ik <111747295+David-Ivanov@users.noreply.github.com>
2023-10-24 12:08:24 +0300

    finish

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function filterArray(numbers, value) {
+    let finalyArray = [];
+    for (const i of numbers) {
+        if (i > value) {
+            finalyArray.push(i);
+        }
+    }
+
+    return finalyArray;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
